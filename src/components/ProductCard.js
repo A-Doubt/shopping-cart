55d2a48164799c9fd35ab79e1298a3e0ae9cdf9a
A-Doubt <adoubt2@gmail.com>
2022-04-03 17:38:22 +0200

    Added the following:
    1. product cards
    2. input with product quantity in each cards
    3. ^input validation
    4. + and - for adding/subtracting 1 from the input
    5. some styling

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,45 @@
-export default function ProductCard() {
+import React from "react"
+
+export default function ProductCard(props) {
+
+	const [quantity, setQuantity] = React.useState(0)
+
+	function handleChange(e) {
+		setQuantity(parseInt(e.target.value, 10));
+	}
+
+	function addQuantity() {
+		setQuantity((prevQuantity) => prevQuantity = prevQuantity + 1);
+	}
+
+	function subtractQuantity() {
+		setQuantity((prevQuantity) => prevQuantity = prevQuantity - 1);
+	}
+
+	// makes quantity a valid 0-999 integer
+	React.useEffect(() => {
+		console.log('from useEffect')
+		if (quantity > 999) return setQuantity(999)
+		if (quantity < 0 || quantity === '' || isNaN(quantity)) return setQuantity(0)
+	}, [quantity])
+
 	return (
-		<div className="card">
-			<h3>Product name</h3>
+		<div className="card flex-column">
+			<h3>{props.name}</h3>
+			<img className="card--image" src={props.imgsrc} alt=''></img>
+			<p>€{props.price}</p>
+			{/* <p>{props.description}</p> */}
+			<div>
+				<button className="card--quantity-btn" onClick={subtractQuantity}>-</button>
+				<input pattern="[0-9]*" value={quantity} onChange={handleChange}></input>
+				<button className="card--quantity-btn" onClick={addQuantity}>+</button>
+			</div>
+			<button 
+				type="button" 
+				onClick={(e) => props.handleClick(e, props.id, quantity)}
+			>
+				Add to cart
+			</button>
 		</div>
 	)
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,30 +1,69 @@
+import React from 'react';
 import Header from './Header';
 import ProductCard from './ProductCard';
+import { nanoid } from 'nanoid';
 
 function Shop() {
+
+	const [itemsData, setItemsData] = React.useState([]);
+	const [cartItems, setCartItems] = React.useState([]);
+	
+	function addToCart(e, id, quantity) {
+		console.log('added to cart');
+		console.log(e);
+		console.log(id);
+		console.log(quantity)
+		if (!quantity) return
+		
+		setCartItems((prevCart) => {
+			return [...prevCart, {
+				id: id,
+				quantity: quantity,
+			}]
+		})
+		console.log('cart: ', cartItems);
+	}
+
+	React.useEffect(() => {
+		fetch('https://fakestoreapi.com/products?limit=15')
+			.then(res => res.json())
+			.then(data => setItemsData(data))
+	}, [])
+	console.log(itemsData);
+
+	const productElements = itemsData.map((ele) => {
+		return (
+			<ProductCard 
+			name={ele.title} 
+			imgsrc={ele.image}
+			price={ele.price.toFixed(2)}
+			handleClick={addToCart}
+			key={nanoid()}
+			id={ele.id}
+			/>
+		)
+	})
+
 	return (
 		<div>
 			<Header />
 			<section  className="shop">
 				<h1>SHOP</h1>
 				<div className='shop--cards'>
+					{productElements}
+					{/* <ProductCard 
+						name={data[0].item.name} 
+						imgsrc={data[0].item.icon_large}
+						price={data[0].item.current.price}
+						description={data[0].item.description}
+					/>
 					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
+					<ProductCard /> */}
 				</div>
 			</section>
 		</div>
 	)
 }
+
 
 export default Shop;
