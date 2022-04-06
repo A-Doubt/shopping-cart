a932b9b82150d7124cd0fa140bb81a5e548306ba
A_Doubt <adoubt2@gmail.com>
2022-04-06 12:30:10 +0200

    Laptop work, shop filters + styling.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,13 +5,50 @@ import ShopPopup from './ShopPopup';
 
 function Shop(props) {
 	const [popupVisible, setPopupVisible] = React.useState(false);
+	const [filters, setFilters] = React.useState({
+		men: true, 
+		women: true
+	});
+	const [visibleItems, setVisibleItems] = React.useState(props.itemsData)
+
+	React.useEffect(() => {
+		setVisibleItems(() => {
+			let newState = [];
+			props.itemsData.forEach((ele) => {
+				if((filters.men && ele.category === 'men\'s clothing')) {
+					newState.push(ele);
+				} else if (filters.women && ele.category === 'women\'s clothing') {
+					newState.push(ele);
+				}
+			});
+			return newState;
+		});
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [filters.men, filters.women]);
+
 
 	function handleClickPopup(e) {
 		props.handleCartFull()
 		setPopupVisible(false);
 	}
 
-	const productElements = props.itemsData.map((ele) => {
+	function handleCheckbox(e) {
+		setFilters((oldState) => {
+			if (e.target.value === 'men') {
+				return {
+					...oldState,
+					men: !oldState.men
+				}
+			} else {
+				return {
+					...oldState,
+					women: !oldState.women
+				}
+			}
+		})
+	}
+
+	const productElements = visibleItems.map((ele) => {
 		return (
 			<ProductCard
 				name={ele.title}
@@ -40,11 +77,27 @@ function Shop(props) {
 				<h1>Browse our products</h1>
 				<div className="shop--main">
 					<div className="filters">
-						Categories
-						<label htmlFor="men">Men</label>
-						<input type="checkbox" id="men"></input>
-						<label htmlFor="women">Women</label>
-						<input type="checkbox" id="women"></input>
+						<h3>Categories</h3>
+						<div className="filter">
+							<input 
+								type="checkbox" 
+								id="men"
+								value="men"
+								checked={filters.men}
+								onChange={handleCheckbox}>
+							</input>
+							<label htmlFor="men">Men</label>
+						</div>
+						<div className="filter">
+							<input 
+								type="checkbox" 
+								id="women"
+								value="women"
+								checked={filters.women}
+								onChange={handleCheckbox}>
+								</input>
+							<label htmlFor="women">Women</label>
+						</div>
 					</div>
 					<div className="shop--cards">{productElements}</div>
 				</div>
